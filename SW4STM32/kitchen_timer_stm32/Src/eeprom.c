e76f2b8228b937c947948816360e57a0bcb441fc
theapi <petermclarke@gmail.com>
2017-03-18 16:04:45 +0000

    Flashed both timers

--- SW4STM32/kitchen_timer_stm32/Src/eeprom.c
+++ SW4STM32/kitchen_timer_stm32/Src/eeprom.c
@@ -1,54 +1,6 @@
 /*
  * @file eeprom.c
  *
- * Address can be one of 15 ( a 4 bit number) addresses.
- * So the data gets saved in 15 different locations,
- * meaning 15 times the normal wear, 150,000 saves instead of 10,000.
- * 1 save a day ~= 411 years
- * 10 saves a day _= 41 years
- * 100 saves a day _= 4 years
- * Should be enough :)
- *
- * So with this each data needs 15 words.
- * The counter is kept in the word previous to the data word, so that's another 15 words.
- * In total the data with it's counter needs 30 words.
- * 30 words for 1 word, we only save a byte, so 30 words for 1 byte!!!
- *
- * EEPROM memory is then:
- * counter | data | counter | data etc...
- *
- * Flash Data EEPROM can hold 2 Kbytes
- *
- * The way it works:
- * - Each save, add 1 to the counter.
- * - Use the counter_address of (id * (32*4)) + (least significant 4 bits of the counter * 8).
- * then the data address is counter_address + 4
- *
- * eg.
- * - First save of data id 0:
- *  id = 0, counter = 0, address = 0 + 0b0000 = 0 + (0 * 8) = 0
- * - Second save:
- *  id = 0, counter = 1, address = 0 + 0b0001 = 0 + (1 * 8) = 8
- * - 15th save:
- *  id = 0, counter = 15, address = 0 + 0b1111 = 0 + (15 * 8) = 120
- * - 123456th save:
- * id = 0, counter = 123456, address = 0 + 0b0000 = 0 + (0 * 8) = 0
- * - 856473564th save:
- * id = 0, counter = 856473564, address = 0 + 0b1100 = 0 + (12 * 8) = 96
- *
- * - First save of data id 1:
- *  id = 1, counter = 0, address = (1 * 128) + 0b0000 = 128 + (0 * 8) = 128
- * - Second save:
- *  id = 1, counter = 1, address = (1 * 128) + 0b0001 = 128 + (1 * 8) = 136
- * - 15th save:
- *  id = 1, counter = 15, address = (1 * 128) + 0b1111 = 128 + (15 * 8) = 248
- * - 123456th save:
- *  id = 1, counter = 123456, address = (1 * 128) + 0b0000 = 128 + (0 * 8) = 128
- * - 856473564th save:
- *  id = 0, counter = 856473564, address = (1 * 128) + 0b1100 = 128 + (12 * 8) = 224
- *
- *  - First save of data id 2:
- *  id = 2, counter = 0, address = (2 * 128) + 0b0000 = 128 + (0 * 4) = 256
  */
 
 #include "eeprom.h"
